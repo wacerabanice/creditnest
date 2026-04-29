50b3233b052977e2b1ab8646724993a695f02086
Wacera <winniebanice@gmail.com>
2026-04-29 21:56:22 +0300

    update

--- creditnest_frontend/src/services/api.js
+++ creditnest_frontend/src/services/api.js
@@ -7,13 +7,13 @@ const API = axios.create({
 });
 
 
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+//API.interceptors.request.use(
+  //(config) => {
+    //const token = localStorage.getItem("token");
+    //if (token) config.headers.Authorization = `Bearer ${token}`;
+    //return config;
+  //},
+  //(error) => Promise.reject(error)
+//);
 
 export default API;
