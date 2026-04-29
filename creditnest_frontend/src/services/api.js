15a7ec92422fd0448993686594e3f8dcbecc894f
Wacera <winniebanice@gmail.com>
2026-04-29 21:15:11 +0300

    update to render postgre

--- creditnest_frontend/src/services/api.js
+++ creditnest_frontend/src/services/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend baseURL
+  //baseURL: "http://localhost:5000/api", // backend baseURL
+  //baseURL: "https://https://creditnest.onrender.com/api"
+  baseURL: process.env.REACT_APP_API_URL,
 });
+
 
 API.interceptors.request.use(
   (config) => {
